c2618d74385d00e2b9be5684413d5d2b3cf5de37
y10e <y10exxx@gmail.com>
2021-02-24 17:57:41 +0900

    release v0.0.1 for EventHubs Sender and Receiver

---
+++ src/EventHubsReceiver/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+using System.Text.Json;
+
+namespace EventHubsReceiver
+{
+
+    class Program
+    {
+        const string version = "0.0.1";
+
+        class Config
+        {
+            public string eventHubName { get; set; }
+            public string eventHubConnectionString { get; set; }
+
+            public string consumergroup { get; set; }
+
+            public string blobStorageConnectionString { get; set; }
+
+            public string blobContainerName { get; set; }
+
+
+            public void ReadConfigFile(string configPath = @"./receiver.config.json")
+            {
+
+                Console.WriteLine("Loading configration setting from {0}", configPath);
+
+                try
+                {
+                    using (StreamReader st = new StreamReader(configPath, Encoding.GetEncoding("UTF-8")))
+                    {
+                        string configJson = st.ReadToEnd();
+                        Config readConfig = JsonSerializer.Deserialize<Config>(configJson);
+
+                        this.eventHubName = readConfig.eventHubName;
+                        this.eventHubConnectionString = readConfig.eventHubConnectionString;
+                        this.consumergroup = readConfig.consumergroup;
+                        this.blobStorageConnectionString = readConfig.blobStorageConnectionString;
+                        this.blobContainerName = readConfig.blobContainerName;
+
+                        if (this.consumergroup == "")
+                        {
+                            this.consumergroup = EventHubConsumerClient.DefaultConsumerGroupName;
+                        }
+
+                        if (this.blobContainerName == "" ) {
+
+                            if (readConfig.consumergroup == "") {
+                                this.blobContainerName = "default";
+                            }
+                            else
+                            {
+                                this.blobContainerName = this.consumergroup;
+                            }
+                        }
+                    }
+
+                    Console.WriteLine("-----------------");
+                    this.PrintValue();
+                    Console.WriteLine("-----------------");
+                    Console.WriteLine("done!");
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("Failed to read config file.");
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            public void PrintValue()
+            {
+                Console.WriteLine("EventHub \t\t\t: {0}", eventHubName);
+                Console.WriteLine("EventHubConnectionString \t: {0}", eventHubConnectionString);
+                Console.WriteLine("consumergroup  \t\t\t: {0}", consumergroup);
+                Console.WriteLine("StorageConnectionString  \t: {0}", blobStorageConnectionString);
+                Console.WriteLine("BlobContainer  \t\t\t: {0}", blobContainerName);
+
+            }
+        }
+
+        static async Task Main()
+        {
+            DisplayAbstract();
+
+            //Load configration from ./receiver.config.json
+            Config config = new Config();
+            config.ReadConfigFile();
+
+            // Create a blob container client that the event processor will use 
+            BlobContainerClient storageClient = new BlobContainerClient(config.blobStorageConnectionString, config.blobContainerName);
+            storageClient.CreateIfNotExists();
+
+            // Create an event processor client to process events in the event hub
+            EventProcessorClient processor = new EventProcessorClient(storageClient, config.consumergroup, config.eventHubConnectionString, config.eventHubName);
+
+            // Register handlers for processing events and handling errors
+            processor.ProcessEventAsync += ProcessEventHandler;
+            processor.ProcessErrorAsync += ProcessErrorHandler;
+
+            // Start the processing
+            Console.WriteLine("Starting to receive messages from Eventhub. Press any key to stop this application.");
+            Console.WriteLine("");
+
+            await processor.StartProcessingAsync();
+
+            // Wait for 10 seconds for the events to be processed
+            //await Task.Delay(TimeSpan.FromSeconds(60));
+            Console.ReadLine();
+
+            // Stop the processing
+            Console.WriteLine("Stopping. please wait a minute.");
+            await processor.StopProcessingAsync();
+        }
+
+        static async Task ProcessEventHandler(ProcessEventArgs eventArgs)
+        {
+            // Write the body of the event to the console window
+            Console.WriteLine("Received event on {0}", DateTime.Now.ToString());
+            Console.WriteLine(" Event Body: {0}", Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()));
+            Console.WriteLine(" EnqueuedTime: {0}", eventArgs.Data.EnqueuedTime.ToString());
+            Console.WriteLine(" SequenceNumber: {0}", eventArgs.Data.SequenceNumber.ToString());
+            Console.WriteLine(" Offset: {0}", eventArgs.Data.Offset.ToString());
+            Console.WriteLine(" PartitionKey: {0}", eventArgs.Partition.PartitionId);
+            Console.WriteLine("");
+
+
+
+            // Update checkpoint in the blob storage so that the app receives only new events the next time it's run
+            await eventArgs.UpdateCheckpointAsync(eventArgs.CancellationToken);
+        }
+
+        static Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+        {
+            // Write details about the error to the console window
+            Console.WriteLine($"\tPartition '{ eventArgs.PartitionId}': an unhandled exception was encountered. This was not expected to happen.");
+            Console.WriteLine(eventArgs.Exception.Message);
+            return Task.CompletedTask;
+        }
+
+        static void DisplayAbstract()
+        {
+            Console.WriteLine(Figgle.FiggleFonts.Standard.Render("EventHub Receiver!"));
+            Console.WriteLine("EventHubReceiver v{0} - simple receive messages utility From EventHub", version);
+            Console.WriteLine("Copyright (C) Yusuke.Yoneda");
+            Console.WriteLine("");
+
+        }
+    }
+}
---
+++ src/EventHubsSender/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using System.Text.Json;
+
+namespace EventHubsSender
+{
+    class Program
+    {
+        const string version = "0.0.1";
+
+        class Config
+        {
+            public int count { get; set; }
+            public string msgprefix { get; set; }
+            public string eventHubName { get; set; }
+            public string connectionString { get; set; }
+
+
+            public Config() {
+                this.count = 10;
+                this.msgprefix = "";
+                this.eventHubName = "";
+                this.connectionString = "";
+            }
+
+            public void ReadConfigFile(string configPath = @"./sender.config.json")
+            {
+                try
+                {
+                    using (StreamReader st = new StreamReader(configPath, Encoding.GetEncoding("UTF-8")))
+                    {
+                        string configJson = st.ReadToEnd();
+                        Config readConfig = JsonSerializer.Deserialize<Config>(configJson);
+                        this.count = readConfig.count;
+                        this.msgprefix = readConfig.msgprefix;
+                        this.eventHubName = readConfig.eventHubName;
+                        this.connectionString = readConfig.connectionString;
+
+                    }
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("Failed to read config file.");
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            public void ReadCmdArgs(string[] cmdArgs)
+            {
+                for (int i = 1; i < cmdArgs.Length; i++)
+                {
+                    //Console.WriteLine("command : {0:d} : {1}\r\n", i, Commands[i]);
+                    switch (cmdArgs[i])
+                    {
+                        // the number of messages sent to EventHub
+                        case "-c":
+                            this.count = int.Parse(cmdArgs[i + 1]);
+                            break;
+                        case "--count":
+                            this.count = int.Parse(cmdArgs[i + 1]);
+                            break;
+
+                        // message prefix
+                        case "-p":
+                            this.msgprefix = cmdArgs[i + 1];
+                            break;
+                        case "--prefix":
+                            this.msgprefix = cmdArgs[i + 1];
+                            break;
+
+                        // EventHub name for the destination
+                        case "-n":
+                            this.eventHubName = cmdArgs[i + 1];
+                            break;
+                        case "--name":
+                            this.eventHubName = cmdArgs[i + 1];
+                            break;
+
+                        // connection string of EventHub
+                        case "-s":
+                            this.connectionString = cmdArgs[i + 1];
+                            break;
+                        case "--connectionstring":
+                            this.connectionString = cmdArgs[i + 1];
+                            break;
+                    }
+                }
+            }
+
+            public void PrintValue() {
+                Console.WriteLine("count \t\t\t: {0}", count);
+                Console.WriteLine("msgprefix \t\t: {0}", msgprefix);
+                Console.WriteLine("eventHubName \t\t: {0}", eventHubName);
+                Console.WriteLine("connectionString \t: {0}", connectionString);
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            string[] Commands = System.Environment.GetCommandLineArgs();
+
+            // Configration initialize
+            Config config = new Config();
+
+            // Read configration value from ./config.json
+            config.ReadConfigFile();
+
+            if (Commands.Length < 2)
+            {
+                DisplayAbstract();
+                return;
+            }
+            else
+            {
+                // Read configration values from ./sender.config.json
+                config.ReadCmdArgs(Commands);
+            }
+
+            if (config.connectionString != "") {
+                Console.WriteLine("-----------------");
+                config.PrintValue();
+                Console.WriteLine("-----------------");
+                await using (var producerClient = new EventHubProducerClient(config.connectionString, config.eventHubName))
+                {
+                    // Create a batch of events 
+                    using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
+
+                    // Add events to the batch. An event is a represented by a collection of bytes and metadata. 
+                    Console.WriteLine("A batch of events started to be published.");
+                    for (int i = 0; i < config.count; i++)
+                    {
+                        String eventmsg = config.msgprefix + " event " + (i+1) +"/"+ config.count + " " + DateTime.Now.ToString();
+                        Console.WriteLine(eventmsg);
+                        eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes(eventmsg)));
+                    }
+
+                    // Use the producer client to send the batch of events to the event hub
+                    await producerClient.SendAsync(eventBatch);
+                    Console.WriteLine("A batch of events has been published.");
+                }
+            }
+
+        }
+
+        static void DisplayAbstract()
+        {
+
+            Console.WriteLine(Figgle.FiggleFonts.Standard.Render("EventHub Sender!"));
+            Console.WriteLine("EventHubSender v{0} - simple messages sending utility to EventHub", version);
+            Console.WriteLine("Copyright (C) Yusuke.Yoneda");
+            Console.WriteLine("");
+            Console.WriteLine("EventHubSender usage: ehsend [-c/--count MessageCount] [-p/--prefix MessagePrefix] [-n/--name EventHubName] [-s/--connectionstring ConnectionString]");
+            Console.WriteLine(" -c\tthe number of messages sent to EventHub. default value is 10.");
+            Console.WriteLine(" -p\tmessage prefix. If Prefix is \"hoge\", the mssage is \"hoge event #{num} yyyy/mm/dd hh:mm:ss");
+            Console.WriteLine(" -n\teventhub name for the destination");
+            Console.WriteLine(" -s\tconnection string of EventHub");
+
+        }
+    }
+}
